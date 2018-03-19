febc93535679251fe199607b879d5e4ae0afc281
cmarchena <cmarchena77@gmail.com>
2018-03-19 16:43:51 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="prueba-scss">
+          <p>Prueba SCSS</p>
+        </div>
+        <div className="prueba-scss-2">
+          <p>Prueba SCSS 2</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
